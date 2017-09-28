e9b945d88f583d82a2729a5d5c1a6b345dd5f0d3
Eric F <ericdfanning@gmail.com>
2017-09-28 14:46:21 -0500

    fixed ajax url path

--- client/model/ebayData.js
+++ client/model/ebayData.js
@@ -1,11 +1,10 @@
 import $ from 'jquery';
 
 export const getData = function(cb) {
-  console.log('production', process.env.NODE_ENV)
-  // process.env.NODE_ENV === 'production' ? 'http://thebairdata.com/getstuff': 
+  console.log('production', process.env.NODE_ENV) 
   $.ajax({
     method: 'GET',
-    url: 'http://127.0.0.1:8000/getStuff',
+    url: process.env.NODE_ENV === 'production' ? 'http://thebairdata.com/getstuff': 'http://127.0.0.1:8000/getStuff',
     success: (data) => {
       console.log('back from the server inside of ebay ajax call success')
       cb(null, data)
