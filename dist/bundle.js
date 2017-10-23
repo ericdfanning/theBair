ec113ddeff15f4311633a9b8dbdce0dfedb25228
Eric F <ericdfanning@gmail.com>
2017-10-22 23:14:28 -0500

    working on more categories

--- dist/bundle.js
+++ dist/bundle.js
@@ -13757,6 +13757,26 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var cats = [{
+	title: 'Dresses',
+	lwrCase: 'dresses'
+}, {
+	title: 'T-Shirts',
+	lwrCase: 'tshirts'
+}, {
+	title: 'Tops & Blouses',
+	lwrCase: 'topsAndBlouses'
+}, {
+	title: 'Flats',
+	lwrCase: 'flats'
+}, {
+	title: 'Sweaters',
+	lwrCase: 'sweaters'
+}, {
+	title: 'Jeans',
+	lwrCase: 'jeans'
+}];
+
 var Main = function (_React$Component) {
 	_inherits(Main, _React$Component);
 
