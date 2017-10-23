2c073d26efe62b0e9e91548fd2245ea5446f14df
Eric F <ericdfanning@gmail.com>
2017-10-23 11:09:32 -0500

    changed cron email to run every Sunday instead of every day

--- server/emailService/emailCronJob.js
+++ server/emailService/emailCronJob.js
@@ -14,7 +14,7 @@ var allCategoriesCache = require('../../cache/allCategoriesCache.js');
   // if the server ever dies, it will simply restart with the server,
   // so at most one day will be lost, and an email won't be sent for the day
   let job = new CronJob({
-    cronTime: '00 00 07 1-31 1-12 0-6',
+    cronTime: '00 00 07 1-31 1-12 0',
     onTick: scan,
     start: true,
     timeZone: 'America/Los_Angeles'
