07082eb3cd0ffcf0b75e1ede47b21237e2c0ed2e
Eric F <ericdfanning@gmail.com>
2018-02-24 19:24:58 -0600

    updated email cron time

--- server/emailService/emailCronJob.js
+++ server/emailService/emailCronJob.js
@@ -14,7 +14,7 @@ let allCategoriesCache = require('../../cache/allCategoriesCache.js');
   // if the server ever dies, it will simply restart with the server,
   // so at most one day will be lost, and an email won't be sent for the week
   let job = new CronJob({
-    cronTime: '00 00 07 1-31 1-12 0',
+    cronTime: '00 07 07 1-31 1-12 0',
     onTick: scan,
     start: true,
     timeZone: 'America/Los_Angeles'
