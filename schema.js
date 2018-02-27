6cf36b6c75279a6afdc6104d57b0fef7b7b2e289
Eric F <ericdfanning@gmail.com>
2018-02-26 21:43:53 -0600

    added useMongoClient options to mongoose connection uri to remove deprication error

--- schema.js
+++ schema.js
@@ -2,7 +2,18 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 mongoose.Promise = Promise
-mongoose.connect('mongodb://localhost/profitPal');
+
+const options = {
+  useMongoClient: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
+
+mongoose.connect('mongodb://localhost/profitPal', options);
 
 var currentSchema = new Schema({
   category: String,
